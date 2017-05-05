0c557e449ca98befb563c218c6fd7484b73ab3ea
Nick Sorrell <nicksorrell@gmail.com>
2017-05-05 15:31:17 -0400

    Remove clutter
    app.js comment and old node data file

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,6 @@
 "use strict";
 import UI from './UI';
 
-/*document.getElementById('begin').addEventListener('click', function() {
-  UI.init();
-}, false);*/
-
 window.onload = function(){
   UI.init();
 }
